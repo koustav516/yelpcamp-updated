597555af596b5472b331b66251681b42002b5b0e
koustav516 <koustav516@gmail.com>
2025-06-06 11:40:07 +0530

    Added models and seedData

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Campground = require("./models/campground");
 
-const PORT = 8080;
+const app = express();
+
+const PORT = 3000;
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/yelp-camp")
+    .then(() => console.log("Db Connected!"))
+    .catch((err) => {
+        console.log("Db Connectoion Error");
+        console.log(err);
+    });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
---
+++ seeds/index.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelper");
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/yelp-camp")
+    .then(() => console.log("Db Connected!"))
+    .catch((err) => {
+        console.log("Db Connectoion Error");
+        console.log(err);
+    });
+
+const randomArr = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${randomArr(descriptors)} ${randomArr(places)}`,
+        });
+        await camp.save();
+    }
+};
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
