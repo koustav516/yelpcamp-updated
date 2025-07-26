943e08e2d6b5374c3034f2ce5c4630ca9c6d92e9
koustav516 <koustav516@gmail.com>
2025-07-26 17:26:11 +0530

    Set Up Sessions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const morgan = require("morgan");
+const session = require("express-session");
 const ExpressError = require("./utils/ExpressError");
 
 const campgrounds = require("./routes/campgrounds");
@@ -28,6 +29,19 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(morgan("tiny")); //logger
+app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+    secret: "thisshouldbeasecret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7, //1 week timing
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+};
+app.use(session(sessionConfig));
 
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
